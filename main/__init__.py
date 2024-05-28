dd0eac2dc1129709a07a3675df15b15e0bc23eea
Raz-KI <89061987+Raz-KI@users.noreply.github.com>
2024-05-28 11:27:57 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
@@ -6,7 +7,7 @@ import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='43645dfsgfddfgsfgsf'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get("DATABASE_URL")
 
 
 db =SQLAlchemy(app)
